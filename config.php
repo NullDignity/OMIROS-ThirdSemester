c3d148818c788806c4fc802756f12c34d5bf6318
Dignity <81301894+NullDignity@users.noreply.github.com>
2025-02-25 12:34:14 +0200

    Delete config.php

--- config.php
+++
@@ -1,7 +0,0 @@
-<?php
-   define('DB_SERVER', 'localhost');
-   define('DB_USERNAME', 'root');
-   define('DB_PASSWORD', 'asriel');
-   define('DB_DATABASE', 'Efarmogi');
-   $db = new mysqli(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE) or die("Connect failed: %s\n". $db -> error);
-?>
